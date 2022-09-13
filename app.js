2c3b80fcac02f25ae537e0b35c0f0d12016cc4c4
ajiths10 <ajithsnair10@gmail.com>
2022-09-13 22:05:43 +0530

    Inital setup node

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const express = require('express');
-
-const app = express();
-
-app.listen(4000);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 4000;
+app.listen(port);
