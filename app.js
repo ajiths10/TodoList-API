05558338d5f83c1a0e6645fb99e2adcbe70628b1
ajiths10 <ajithsnair10@gmail.com>
2022-09-13 22:44:36 +0530

    Update, Delete API added

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const port = 4000;
+app.use(body_parser_1.default.json());
+app.use();
 app.listen(port);
--- routes/todos.ts
+++ routes/todos.ts
@@ -1,23 +1,40 @@
-import {  Router } from 'express';
+import { Router } from "express";
 
 const router = Router();
 
 import { Todo } from "../models/todo";
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({ todos : todos})
+router.get("/", (req, res, next) => {
+  res.status(200).json({ todos: todos });
 });
 
-router.post('/todo',(req, res, next) => {
-    const newTodo: Todo = {
-        id: new Date().toISOString(),
-        text: req.body.text 
-    };
+router.post("/todo", (req, res, next) => {
+  const newTodo: Todo = {
+    id: new Date().toISOString(),
+    text: req.body.text,
+  };
 
-    todos.push(newTodo);
-    
-    res.status(200).json({ todos : todos})
+  todos.push(newTodo);
+
+  res.status(200).json({ message: " Aadded", todos: todos });
+});
+
+router.put("/todo/:todoId", (req, res, next) => {
+  const tid = req.params.todoId;
+  const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
+  if (todoIndex >= 0) {
+    todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+    return res.status(200).json({ message: " Updated", todos: todos });
+  }
+  res.status(404).json({ message: "Error in fetching !!" });
+});
+
+
+router.delete('/todo/:todoId', (req, res, next) => {
+    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+    return res.status(200).json({ message: " Deleted", todos: todos });
 })
+
 export default router;
