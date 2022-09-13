c08f98021a976a61b7e5c8839fe6ed1738c98359
ajiths10 <ajithsnair10@gmail.com>
2022-09-13 22:24:32 +0530

    get and post added

--- app.ts
+++ app.ts
@@ -1,5 +1,12 @@
 import  express  from "express";
+import bodyParser from "body-parser";
+
 const app = express();
 const port = 4000;
+
+import todosRoutes from './routes/todos';
+
+app.use(bodyParser.json());
+app.use()
 
 app.listen(port);
